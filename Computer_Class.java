d1e952723adb68e5f0f72242f6b4687147ec8e4f
luxuriouscoder26 <76402362+luxuriouscoder26@users.noreply.github.com>
2021-05-23 11:49:03 +0530

    Add files via upload

---
+++ Computer_Class.java
@@ -0,0 +1,6 @@
+public class Computer_Class {
+    public static void main() {
+        int a;
+        System.out.println("Hello World!");
+    }
+}
---
+++ Something.py
@@ -0,0 +1,3 @@
+weight_g = input('What is your weight in grams? ')
+weight_kg = int(weight_g) / 1000
+print(weight_kg)
